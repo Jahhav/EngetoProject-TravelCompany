d39ac2dd735833eaca9dc914759628f60c9fc337
Jan Havlat <jan.havlath@gmail.com>
2024-06-09 03:27:19 +0200

    renamed default path home

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/EngetoProject" element={<Home />} />
         <Route path="/api" element={<Api />} />
       </Routes>
     </BrowserRouter>
